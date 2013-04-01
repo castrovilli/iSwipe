4a64db8ed5f865cce48c1b8f312a89c83517cc25
tediscript <tediscript@gmail.com>
2013-04-01 12:11:18 +0700

    Initial Commit

---
+++ iSwipe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iSwipe
+//
+//  Created by Tedi Fibri on 4/1/13.
+//  Copyright (c) 2013 tediscript. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
